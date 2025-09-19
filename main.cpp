acdd6213a82f1d7c702632fca53917f861a33d8d
Lewis Scott <Ec2078156@edinburghcollege.ac.uk>
2025-09-19 18:26:17 +0100

    added paddles and ball to screen. one paddle is now movable but still needs clamped to screen.

--- main.cpp
+++ main.cpp
@@ -1,39 +1,103 @@
 #include <SFML/Graphics.hpp>
+using namespace sf;
 
-void init() {
+const sf::Keyboard::Key controls[4] = 
+{
+	Keyboard::A,   // Player1 UP
+	Keyboard::Z,   // Player1 Down
+	Keyboard::Up,  // Player2 UP
+	Keyboard::Down // Player2 Down
+};
+
+//Parameters
+const Vector2f _paddleSize(25.f, 100.f);
+const float _ballRadius = 10.f;
+const int _gameWidth = 800;
+const int _gameHeight = 600;
+const float _paddleSpeed = 400.f;
+const float _paddleOffsetWall = 10.f;
+const float _timeStep = 0.017f; //60 fps
+
+//Objects of the game
+CircleShape _ball;
+RectangleShape _paddles[2];
+
+void Init() {
 	// initialise all the objects needed for the game. 
+	
+	// Set size and origin of paddles
+	for (RectangleShape& p : _paddles) {
+		p.setSize(_paddleSize);
+		p.setOrigin(_paddleSize / 2.f);
+	}
+
+	// Set size and origin of ball
+	_ball.setRadius(_ballRadius);
+	_ball.setOrigin(_ballRadius, _ballRadius); //Should be half the ball width and height
+
+	// reset paddle position
+	_paddles[0].setPosition(_paddleOffsetWall + _paddleSize.x / 2.f, _gameHeight / 2.f);
+	_paddles[1].setPosition(_gameWidth - _paddleOffsetWall - _paddleSize.x / 2.f, _gameHeight / 2.f);
+
+	// reset Ball Position
+	_ball.setPosition(_gameWidth / 2.f, _gameHeight / 2.f);
+	
 }
 
-void update(float dt) {
+void Update(float dt) {
 	// Update Everything
+
+	// handle paddle movement
+	float direction = 0.0f;
+
+	if (Keyboard::isKeyPressed(controls[0])) {
+		direction--;
+	}
+	if (Keyboard::isKeyPressed(controls[1])) {
+		direction++;
+	}
+
+	_paddles[0].move(sf::Vector2f(0.f, direction * _paddleSpeed * dt));
+
 }
 
-void render(sf::RenderWindow& window) {
+void Render(RenderWindow& window) {
 	// Draw Everything
+	window.draw(_paddles[0]);
+	window.draw(_paddles[1]);
+	window.draw(_ball);
 }
 
-void clean() {
+void Clean() {
 	//free up the memory if necessary.
 }
 
 int main() {
 	//create the window
-	sf::RenderWindow window(sf::VideoMode({ 600, 300 }), "PONG");
+	RenderWindow window(VideoMode({ _gameWidth, _gameHeight }), "PONG");
+	// turn on vsync to limit the frame rate to the refresh rate of the monitor
+	window.setVerticalSyncEnabled(true);
 	//initialise and load
-	init();
-	while (!window.isOpen()) {
+	Init();
+
+	// Game loop runs while window is open
+	while (window.isOpen()) 
+	{
 		//Calculate dt
 		static sf::Clock clock;
 		const float dt = clock.restart().asSeconds();
 
+		// clear previous display
 		window.clear();
-		update(dt);
-		render(window);
+
+		Update(dt);
+		Render(window);
 		//wait for the time_step to finish before displaying the next frame.
-		sf::sleep(clock.getElapsedTime());
+		sleep(seconds(_timeStep));
 		//Wait for Vsync
 		window.display();
 	}
+
 	//Unload and shutdown
-	clean();
+	Clean();
 }
